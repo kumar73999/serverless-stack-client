881733c34b7e4ece42d87737c19ec2b1c2e8c3c8
kumar73999 <kumar73999@gmail.com>
2020-02-19 07:18:57 -0500

    some more changes

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notess-app"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://eqih1srmuf.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_lWYTVQu6U",
+      APP_CLIENT_ID: "7j5jnastlnmj9d29ghf63t7t51",
+      IDENTITY_POOL_ID: "us-east-1:4cf8c6b7-1d2b-4db4-a11f-450bf3797c84"
+    }
+  };
